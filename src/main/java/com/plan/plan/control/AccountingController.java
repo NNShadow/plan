f7e953f4e81f27adbad7a2f2ce2bedd902ba4fda
NNShadow <1581333251@qq.com>
2019-05-04 22:44:27 +0800

    with MySQL

--- src/main/java/com/plan/plan/control/AccountingController.java
+++ src/main/java/com/plan/plan/control/AccountingController.java
@@ -4,6 +4,7 @@ import com.plan.plan.model.AccountingRecord;
 import com.plan.plan.service.impl.RecordServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.Date;
@@ -24,21 +25,24 @@ public class AccountingController {
         user.setModifyTime(time);
         user.setDisplay(1);
 
+        //以下信息可以前端写，忘了，懒的改了
+
         //验证金额
-        if(!check(String.valueOf(user.getCash()),  "^?\\d+(\\.\\d+)?$")){
-            System.out.println("错误1");
-            return "recordReturn";
+        if(!check(user.getCash() + "",  "^(\\d+)(\\.\\d{1,2})?$")){
+            System.out.println("金额输入错误");
+            return "error/recordError";
         }
 
         //验证详细信息
-        if(user.getCategory() == null){
-            System.out.println("错误2");
-            return "recordReturn";
+        //double类型无法存储字符串，可以重写400页面
+        if(check(user.getCategory(), "^(\\s+)?$")){
+            System.out.println("缺少内容");
+            return "error/recordError";
         }
         //验证产生时间
         if(!check(user.getCreateTime(), "^(\\d{4})(-)(\\d{2})(-)(\\d{2})$")){
-            System.out.println("错误3");
-            return "recordReturn";
+            System.out.println("日期输入错误");
+            return "error/recordError";
         }
 
         userService.save(user);
